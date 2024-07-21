f95866403a9bd0d66c839b7aa84e8f0c4101ce96
Eduardo <ehernandezvilla@gmail.com>
2024-07-21 15:19:38 -0400

    adding dealership and nltk

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -46,6 +46,16 @@ def get_cars(request):
         cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
     return JsonResponse({"CarModels": cars})
 
+
+#Update the `get_dealerships` render list of dealerships all by default, particular state if state is passed
+def get_dealerships(request, state="All"):
+    if(state == "All"):
+        endpoint = "/fetchDealers"
+    else:
+        endpoint = "/fetchDealers/"+state
+    dealerships = get_request(endpoint)
+    return JsonResponse({"status":200,"dealers":dealerships})
+
 # Create a `login_request` view to handle sign in request
 @csrf_exempt
 def login_user(request):
