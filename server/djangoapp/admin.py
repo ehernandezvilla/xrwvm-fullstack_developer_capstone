997802e82275bd56377705c7b54246a87213ef7b
Eduardo <ehernandezvilla@gmail.com>
2024-07-21 00:33:23 -0400

    adding new models

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,17 @@
-# from django.contrib import admin
-# from .models import related models
-
+from django.contrib import admin
+from .models import CarMake, CarModel
 
 # Register your models here.
 
 # CarModelInline class
-
-# CarModelAdmin class
+class CarModelInline(admin.TabularInline):
+    model = CarModel
+    extra = 1
 
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
 
 # Register models here
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel)
