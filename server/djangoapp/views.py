be766bdc54851c7697a9e951bb41fcf827bb681d
Eduardo <ehernandezvilla@gmail.com>
2024-07-21 14:27:10 -0400

    adding car make and models

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,46 +1,67 @@
-# Uncomment the required imports before adding the code
-
 from django.shortcuts import render
-from django.http import HttpResponseRedirect, HttpResponse
-from django.contrib.auth.models import User
-from django.shortcuts import get_object_or_404, render, redirect
-from django.contrib.auth import logout
-from django.contrib import messages
-from datetime import datetime
-
 from django.http import JsonResponse
 from django.contrib.auth import login, authenticate
 import logging
 import json
 from django.views.decorators.csrf import csrf_exempt
-# from .populate import initiate
-
+from .models import CarMake, CarModel
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
 
+def initiate():
+    # Example data to populate the database
+    car_makes = [
+        {'name': 'Toyota', 'description': 'Japanese car manufacturer'},
+        {'name': 'Ford', 'description': 'American car manufacturer'},
+    ]
+
+    car_models = [
+        {'name': 'Camry', 'car_make': 'Toyota', 'type': 'SEDAN', 'year': 2023},
+        {'name': 'Corolla', 'car_make': 'Toyota', 'type': 'SEDAN', 'year': 2022},
+        {'name': 'F-150', 'car_make': 'Ford', 'type': 'SUV', 'year': 2021},
+    ]
+
+    for car_make in car_makes:
+        CarMake.objects.create(name=car_make['name'], description=car_make['description'])
+
+    for car_model in car_models:
+        car_make = CarMake.objects.get(name=car_model['car_make'])
+        CarModel.objects.create(
+            name=car_model['name'],
+            car_make=car_make,
+            type=car_model['type'],
+            year=car_model['year']
+        )
 
 # Create your views here.
+def get_cars(request):
+    count = CarMake.objects.filter().count()
+    print(count)
+    if count == 0:
+        initiate()
+    car_models = CarModel.objects.select_related('car_make')
+    cars = []
+    for car_model in car_models:
+        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+    return JsonResponse({"CarModels": cars})
 
 # Create a `login_request` view to handle sign in request
 @csrf_exempt
 def login_user(request):
-    # Get username and password from request.POST dictionary
     data = json.loads(request.body)
     username = data['userName']
     password = data['password']
-    # Try to check if provide credential can be authenticated
     user = authenticate(username=username, password=password)
     data = {"userName": username}
     if user is not None:
-        # If user is valid, call login method to login current user
         login(request, user)
         data = {"userName": username, "status": "Authenticated"}
     return JsonResponse(data)
 
 # Create a `logout_request` view to handle sign out request
 def logout_request(request):
-    data = {"userName":""}
+    data = {"userName": ""}
     return JsonResponse(data)
 
 # Create a `registration` view to handle sign up request
@@ -56,37 +77,15 @@ def registration(request):
     username_exist = False
     email_exist = False
     try:
-        # Check if user already exists
         User.objects.get(username=username)
         username_exist = True
     except:
-        # If not, simply log this is a new user
         logger.debug("{} is new user".format(username))
-    # If it is a new user
     if not username_exist:
-        # Create user in auth_user table
-        user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name,password=password, email=email)
-        # Login the user and redirect to list page
+        user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name, password=password, email=email)
         login(request, user)
-        data = {"userName":username,"status":"Authenticated"}
+        data = {"userName": username, "status": "Authenticated"}
         return JsonResponse(data)
-    else :
-        data = {"userName":username,"error":"Already Registered"}
+    else:
+        data = {"userName": username, "error": "Already Registered"}
         return JsonResponse(data)
-
-# # Update the `get_dealerships` view to render the index page with
-# a list of dealerships
-# def get_dealerships(request):
-# ...
-
-# Create a `get_dealer_reviews` view to render the reviews of a dealer
-# def get_dealer_reviews(request,dealer_id):
-# ...
-
-# Create a `get_dealer_details` view to render the dealer details
-# def get_dealer_details(request, dealer_id):
-# ...
-
-# Create a `add_review` view to submit a review
-# def add_review(request):
-# ...
