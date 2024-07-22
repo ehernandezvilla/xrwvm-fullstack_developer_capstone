9e4e2ce82bdb8aca14d0fa0b6866aab4bd2a4dc2
Eduardo Hernández Villa <ehernandezvilla@gmail.com>
2024-07-22 18:54:22 -0400

    fixing djangoapp/admin.py pep8

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import CarMake, CarModel
 
+
 # Register your models here.
 
 # CarModelInline class
@@ -8,10 +9,13 @@ class CarModelInline(admin.TabularInline):
     model = CarModel
     extra = 1
 
+
 # CarMakeAdmin class with CarModelInline
 class CarMakeAdmin(admin.ModelAdmin):
     inlines = [CarModelInline]
 
+
 # Register models here
 admin.site.register(CarMake, CarMakeAdmin)
 admin.site.register(CarModel)
+
