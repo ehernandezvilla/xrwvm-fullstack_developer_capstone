d2855a4b45fb2ae51cb7401d6120bf1502e0abe5
Eduardo Hernández Villa <ehernandezvilla@gmail.com>
2024-07-23 13:08:30 -0400

    fixing error E501

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -4,7 +4,9 @@ from . import views
 urlpatterns = [
     path('get_cars/', views.get_cars, name='get_cars'),
     path('dealerships/', views.get_dealerships, name='dealerships'),
-    path('dealer/<int:dealer_id>/', views.get_dealer_details, 
+    path(
+        'dealer/<int:dealer_id>/',
+        views.get_dealer_details,
         name='dealer_details'
     ),
     path(
